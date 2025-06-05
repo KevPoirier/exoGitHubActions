84593deeb0b036ba92015e5b599d13bab639a85e
Kevin Poirier <105774471+KevPoirier@users.noreply.github.com>
2025-06-05 14:04:58 +0200

    Update main.go

--- app/cmd/webserver/main.go
+++ app/cmd/webserver/main.go
@@ -9,7 +9,7 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	log.Println("🏁 [INFO] Starting Poker Server version 0.1.8...")
+	log.Println("🏁 [INFO] Starting Poker Server version 0.1.10...")
 	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
